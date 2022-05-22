ca0dcd762715564113c17b89b58a8b647e344393
caique <caique_sarkis_corebiz@whirlpool.com>
2022-05-22 12:04:30 -0300

    testing endpoints

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
-import express from "express";
-const app = express();
-console.log('hello')
-app.get("/", function (req, res) {
-  res.send("Hello World");
-});
- 
-app.listen(3000);
+import express from "express"
+const app = express()
+const port = 3000
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.get('/', (req, res) => {
+  res.send('Wellcome to Mallspawn Api!')
+})
+
+app.get('/products', (req, res) => {
+  res.send(req.query)
+})
+
+app.post('/products/add', (req,res)=>{
+  res.send(req.body)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
