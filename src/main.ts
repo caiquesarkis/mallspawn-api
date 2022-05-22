b0ee8caa7b666222b8195f1922eabab32293f513
caique <caique_sarkis_corebiz@whirlpool.com>
2022-05-22 19:29:39 -0300

    POST for product registry

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,28 @@
 import express from "express"
 import mysql from 'mysql2';
 import 'dotenv/config'
+import { stat } from "fs";
 
 
 const app = express()
-const port = 3000
+const port = process.env.API_PORT || 5000
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 
 const connection = mysql.createConnection({
-  host: 'localhost',
+  host: process.env.DB_HOST,
   password: process.env.DB_PASSWORD,
-  user: 'root',
-  database: 'mallspawn'
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME
 });
 
 
-
+// GET
+app.get('/', (req, res) => {
+  res.send('Wellcome to Mallspawn Api!')
+})
 
 app.get('/stores', (req, res)=>{
   let query = 'SELECT * FROM stores;'
@@ -28,18 +32,26 @@ app.get('/stores', (req, res)=>{
   );
 })
 
-app.get('/', (req, res) => {
-  res.send('Wellcome to Mallspawn Api!')
-})
-
 app.get('/products', (req, res) => {
-  res.send(req.query)
+  const query = 'SELECT * FROM products;'
+  connection.query(query, (err, results, fields) => {
+        res.send(results)
+    }
+  );
 })
 
-app.post('/products/add', (req,res)=>{
-  res.send(req.body)
-})
 
+
+app.post('/products', (req,res)=>{
+  let store = 'Multithings';
+  
+  const {name, description, category, status, price, compareAtPrice, images} = req.body
+  const query = `INSERT INTO products (Store_Name, Name, Description, Category, IsActive, Price, CompareAtPrice, Images) VALUES ("${store}", "${name}", "${description}", "${category}", ${status}, "${price}", "${compareAtPrice}", '["kakak","kaka"]');`
+  connection.query(query, (err, results, fields) => {
+      res.send(err + query)
+    }
+  );
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
