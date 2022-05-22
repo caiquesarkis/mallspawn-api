601fafd7793fe9d513441d70b61a5d303df53b6a
caique <caique_sarkis_corebiz@whirlpool.com>
2022-05-22 18:06:30 -0300

    MySQL database integration

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,29 @@ const port = 3000
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
+// get the client
+import mysql from 'mysql2';
+
+// create the connection to database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  password: "33416738aA!",
+  user: 'root',
+  database: 'mallspawn'
+});
+
+// simple query
+
+
+
+app.get('/stores', (req, res)=>{
+  let query = 'SELECT * FROM stores;'
+  connection.query(query, (err, results, fields) => {
+        res.send(results)
+    }
+  );
+})
+
 app.get('/', (req, res) => {
   res.send('Wellcome to Mallspawn Api!')
 })
@@ -16,6 +39,7 @@ app.get('/products', (req, res) => {
 app.post('/products/add', (req,res)=>{
   res.send(req.body)
 })
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
